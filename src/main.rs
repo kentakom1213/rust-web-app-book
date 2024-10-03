3502c21a61a09e50099102ad5a2bbd8d998268f1
kentakom1213 <kentakom1213@gmail.com>
2024-10-03 13:06:19 +0900

    [add] axumの基本的な使い方

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{Ipv4Addr, SocketAddr};
+
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() {
+    // ルーター
+    let app = Router::new().route("/hello", get(hello_world));
+
+    // ローカルホストの8080番
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+
+    // リスナーを立ち上げる
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    // ログを出力する
+    println!("Listening on {}", addr);
+
+    // サーバーを起動する
+    axum::serve(listener, app).await.unwrap();
 }
