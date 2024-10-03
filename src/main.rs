1038d795563fd4a20e784ada35ad664d879fe721
kentakom1213 <kentakom1213@gmail.com>
2024-10-03 13:17:56 +0900

    [add] エラーハンドリング (-3.3)

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
-use axum::{routing::get, Router};
+use anyhow::Result;
+use axum::{http::StatusCode, routing::get, Router};
 use tokio::net::TcpListener;
 
-async fn hello_world() -> &'static str {
-    "Hello, World!"
+/// ヘルスチェック用のハンドラ
+pub async fn health_check() -> StatusCode {
+    StatusCode::OK
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     // ルーター
-    let app = Router::new().route("/hello", get(hello_world));
+    let app = Router::new().route("/health", get(health_check));
 
     // ローカルホストの8080番
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
 
     // リスナーを立ち上げる
-    let listener = TcpListener::bind(addr).await.unwrap();
+    let listener = TcpListener::bind(addr).await?;
 
     // ログを出力する
     println!("Listening on {}", addr);
 
     // サーバーを起動する
-    axum::serve(listener, app).await.unwrap();
+    Ok(axum::serve(listener, app).await?)
 }
