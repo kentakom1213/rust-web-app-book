ce05cb3ffec5a697f05b0b000f56480e0abc62d9
kentakom1213 <kentakom1213@gmail.com>
2024-10-08 01:41:27 +0900

    [add] アプリの完成

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -1,1 +1,38 @@
-fn main() {}
+use std::net::{Ipv4Addr, SocketAddr};
+
+use adapter::database::connect_database_with;
+use anyhow::{Error, Result};
+use api::route::health::build_health_check_routers;
+use axum::Router;
+use registry::AppRegistry;
+use shared::config::AppConfig;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    bootstrap().await
+}
+
+async fn bootstrap() -> Result<()> {
+    // app_configの生成
+    let app_config = AppConfig::new()?;
+    // データベースへの接続
+    let pool = connect_database_with(&app_config.database);
+
+    // AppRegistryを生成する
+    let registry = AppRegistry::new(pool);
+
+    // build_health_check関数を呼び出す
+    let app = Router::new()
+        // ルーターに登録する
+        .merge(build_health_check_routers())
+        .with_state(registry);
+
+    // サーバーの起動
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    let listener = TcpListener::bind(&addr).await?;
+
+    println!("Listening on {}", addr);
+
+    axum::serve(listener, app).await.map_err(Error::from)
+}
